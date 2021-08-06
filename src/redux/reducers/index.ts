613c841b3abb8be27753e7bc58024415f0ce80e1
Krzysztof Sobiecki <krzysztof.s@niceguys.info>
2021-08-06 13:11:01 +0200

    Fix counter logic

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './userReducer';
+import counterReducer from './counterReducer';
 
 const reducers = combineReducers({
 	user: userReducer,
+	counter: counterReducer,
 });
 
 export default reducers;
