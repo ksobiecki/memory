049a685a14d995f7b742836e7688c7cfee1bf633
Krzysztof Sobiecki <krzysztof.s@niceguys.info>
2021-08-09 13:24:41 +0200

    Add restriction to routes so that you cant enter game and scoreboard pages without entering username

--- src/components/Routes/index.tsx
+++ src/components/Routes/index.tsx
@@ -1,28 +1,31 @@
 import { Suspense } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 import Landing from '../../pages/Landing';
 import Game from '../../pages/Game';
 import Scoreboard from '../../pages/Scoreboard';
 import NotFound from '../../pages/NotFound';
+import { useSelector } from 'react-redux';
+import { State } from '../../redux/reducers';
 
-const Routes = () => (
-	<Suspense fallback={<div>Loading ...</div>}>
-		<Switch>
-			<Route path="/" exact>
-				<Landing />
-			</Route>
-			<Route path="/game">
-				<Game />
-			</Route>
-			<Route path="/scoreboard">
-				<Scoreboard />
-			</Route>
-			<Route path="*">
-				<NotFound />
-			</Route>
-		</Switch>
-	</Suspense>
-);
+const Routes = () => {
+	const username = useSelector((state: State) => state.user);
 
+	return (
+		<Suspense fallback={<div>Loading ...</div>}>
+			<Switch>
+				<Route path="/" exact>
+					<Landing />
+				</Route>
+				<Route path="/game">{username ? <Game /> : <Redirect to="/" />}</Route>
+				<Route path="/scoreboard">
+					{username ? <Scoreboard /> : <Redirect to="/" />}
+				</Route>
+				<Route path="*">
+					<NotFound />
+				</Route>
+			</Switch>
+		</Suspense>
+	);
+};
 export default Routes;
